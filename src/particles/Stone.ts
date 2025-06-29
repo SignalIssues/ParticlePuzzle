f936f57bd0c66d0740bec60afc84528977d83667
SignalIssues <travislongenbach@gmail.com>
2025-06-29 15:31:23 -0400

    Add water and stone particles with options

--- src/particles/Stone.ts
+++ src/particles/Stone.ts
@@ -1,19 +1,19 @@
+import Phaser from 'phaser';
 import { Particle } from './Particle';
 import { ensureCircleTexture } from '../utils/TextureGenerator';
 
-export class Sand extends Particle {
-
+export class Stone extends Particle {
   constructor(scene: Phaser.Scene, x: number, y: number) {
-    ensureCircleTexture(scene, 'sandTexture', 0xc2b280, 3);
-    super(scene, x, y, 'sandTexture', {
+    ensureCircleTexture(scene, 'stoneTexture', 0xcccccc, 3);
+    super(scene, x, y, 'stoneTexture', {
       shape: { type: 'circle', radius: 3 },
-      density: 0.001,
+      density: 0.003,
       restitution: 0,
-      friction: 0.2
+      friction: 0.3
     });
   }
 
   update(delta: number) {
-    // custom behavior (if any) per frame
+    // custom stone behavior can be added here
   }
 }
--- src/particles/Water.ts
+++ src/particles/Water.ts
@@ -1,19 +1,19 @@
+import Phaser from 'phaser';
 import { Particle } from './Particle';
 import { ensureCircleTexture } from '../utils/TextureGenerator';
 
-export class Sand extends Particle {
-
+export class Water extends Particle {
   constructor(scene: Phaser.Scene, x: number, y: number) {
-    ensureCircleTexture(scene, 'sandTexture', 0xc2b280, 3);
-    super(scene, x, y, 'sandTexture', {
+    ensureCircleTexture(scene, 'waterTexture', 0x3399ff, 3);
+    super(scene, x, y, 'waterTexture', {
       shape: { type: 'circle', radius: 3 },
-      density: 0.001,
+      density: 0.0008,
       restitution: 0,
-      friction: 0.2
+      friction: 0.05
     });
   }
 
   update(delta: number) {
-    // custom behavior (if any) per frame
+    // custom water behavior can be added here
   }
 }
